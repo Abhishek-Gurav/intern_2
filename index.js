f44f59db97d21eaad2488cbfc532c3db999b8208
Abhishek-Gurav <79034352+Abhishek-Gurav@users.noreply.github.com>
2022-01-21 20:45:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// api url
+const api_url =
+	"https://run.mocky.io/v3/f3feef1c-9bfa-43fd-b2a0-bbe9abadb4f6";
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		console.log(response);
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+// function hideloader() {
+// 	document.getElementById('loading').style.display = 'none';
+// }
+// Function to define innerHTML for HTML table
+function show(data) {
+    console.log(data);
+	let tab =``;
+	
+	// Loop to access all rows
+	for (let r of data.clients) {
+		tab += `<tr>
+	<td>${r.name} </td>
+	<td>${r.company}</td>		
+</tr>`;
+	}
+	// Setting innerHTML as tab variable
+	document.getElementById("cList").innerHTML = tab;
+}
